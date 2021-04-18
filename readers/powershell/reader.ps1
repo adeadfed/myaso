cd9f3d90e0c03b68f349284eb347e6cf933e7609
harpsiford <kasumi2197@gmail.com>
2021-04-18 16:25:14 +0300

    Fix PS reader

--- readers/powershell/reader.ps1
+++ readers/powershell/reader.ps1
@@ -2,36 +2,17 @@
 [Int32] $length = 128 + 1
 $BitMap = [System.Drawing.Image]::FromFile((Get-Item $filename).fullname, $true)
 
-$bits = New-Object System.Collections.BitArray($length)
+$bytes = New-Object byte[] ($length)
+
+# $bytes
 
 [Int32]$pos = 0
 
-function ConvertToBytes([System.Collections.Bitarray]$bits) {
-    $ret = new-object byte[] ([int][Math]::Floor(($bits.Length - 1) / 8) + 1)
-    $bits.CopyTo($ret, 0)
-    $ret
+function get_lsb([byte]$target, [byte]$source) { 
+    $a = $target -shl 1
+    $b = $source -band 1
+    $a -bor $b
 }
-
-$signature = @"
-[DllImport("Crypt32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-public static extern bool CryptStringToBinary(
-    string pszString,
-    int cchString,
-    int dwFlags,
-    byte[] pbBinary,
-    ref int pcbBinary,
-    int pdwSkip,
-    ref int pdwFlags
-);
-[DllImport("Crypt32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-public static extern bool CryptBinaryToString(
-    byte[] pbBinary,
-    int cbBinary,
-    int dwFlags,
-    StringBuilder pszString,
-    ref int pcchString
-);
-"@
 
 foreach($y in (($BitMap.Height-1)..0)) {
 	foreach($x in (0..($BitMap.Width-1))) {
@@ -42,15 +23,15 @@ foreach($y in (($BitMap.Height-1)..0)) {
 
         # "($B, $G, $R)"
         
-        foreach ($bit in ($Pixel.B, $Pixel.G, $Pixel.R)) {
-            if ($length -le 0) { 
-                foreach ($bit in $bits) { $b = [int]$bit; Write-Host -NoNewline $b }
-                ConvertToBytes($bits)
+        foreach ($byte in ($B, $G, $R)) {
+            if ($length -le 0) {
+                [char[]]$bytes
                 exit
             }
-            # $bit
-            $b = [int]([byte]$bit -band 0x01)
-            $bits.Set($pos, $b)
+
+            $idx = [int][Math]::Floor($pos / 8)
+            $bytes[$idx] = get_lsb $bytes[$idx] $byte
+            
             $pos += 1
             $length -= 1
         }
