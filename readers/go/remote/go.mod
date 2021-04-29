436afdd82bf99cfb60b1be90a8b095c188425a85
harpsiford <kasumi2197@gmail.com>
2021-04-29 23:40:32 +0300

    Reorganize Go files

--- readers/go/remote/go.mod
+++
@@ -1,8 +0,0 @@
-module go_reader_remote
-
-go 1.16
-
-require (
-	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb
-	golang.org/x/sys v0.0.0-20210423082822-04245dca01da
-)
--- readers/go/template.go
+++ readers/go/template.go
@@ -4,7 +4,6 @@ const image_source = "C:/Users/Blackberry/Desktop/projects/yet-another-shellcode
 const shellcode_len = {{ MAX_BITS }}
 
 
-
 func main() {
     var payload_data [shellcode_len / 8]byte
     var image = get_image(image_source)
