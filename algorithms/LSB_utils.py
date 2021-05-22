c78c660922c20353677866e44694b557a4bfb49b
harpsiford <kasumi2197@gmail.com>
2021-05-22 17:25:53 +0300

    Implement 'colorcode'

--- algorithms/LSB_utils.py
+++ algorithms/LSB_utils.py
@@ -1,3 +1,6 @@
+from random import randrange
+
+
 def __set_lsb(byte, value):
     """
     set last bit to 1 by doing bitwise OR with 0b00000001
@@ -10,3 +13,10 @@ def __get_lsb(byte):
     """get last bit by doing bitwise AND with 0b00000001"""
     return byte & 0b1
 
+
+def __from_colorcode(byte):
+    return byte % 128 % 1
+
+
+def __to_colorcode(value):
+    return randrange(175, 255) if value else randrange(0, 100)
--- algorithms/colorcode.py
+++ algorithms/colorcode.py
@@ -2,25 +2,17 @@ from itertools import product
 
 from bitarray import bitarray
 from PIL import Image
-from .LSB_utils import __get_lsb, __set_lsb
-
-
-def capacity(img: Image):
-    """Maximum number of least significant bits"""
-    return img.height * img.width * 3
+from .LSB_utils import __from_colorcode, __to_colorcode
 
 
 def embed(img: Image, payload: bitarray, *args, **kwargs):
-    assert len(payload) <= capacity(img), \
-        f'[-] payload length ({len(payload)}) is greater than image capacity ({capacity(img)})!'
-
     for y, x in product(range(img.height), range(img.width)):
         r, g, b = img.getpixel((x, y))
 
         """set lsb of each color to target value"""
-        if payload: r = __set_lsb(r, payload.pop(0))
-        if payload: g = __set_lsb(g, payload.pop(0))
-        if payload: b = __set_lsb(b, payload.pop(0))
+        if payload: r = __to_colorcode(payload.pop(0))
+        if payload: g = __to_colorcode(payload.pop(0))
+        if payload: b = __to_colorcode(payload.pop(0))
 
         img.putpixel((x, y), (r, g, b))
 
@@ -33,7 +25,7 @@ def extract(img: Image, payload_bits: int, *args, **kwargs) -> bitarray:
             if not payload_bits:
                 break
 
-            bit = __get_lsb(byte)
+            bit = __from_colorcode(byte)
             payload.append(bit)
             payload_bits -= 1
     return payload
