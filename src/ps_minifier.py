b41437e9c6d5158a83781a5265ef54ca137513fb
harpsiford <kasumi2197@gmail.com>
2021-06-15 16:18:01 +0300

    Bugfix: RE for whitespaces; ready PS minifier

--- src/ps_minifier.py
+++ src/ps_minifier.py
@@ -1,47 +1,53 @@
 import re
-from sre_constants import error
 import string
 import random
 
-rand_namespace = string.ascii_letters + string.digits
+from loguru import logger
+
+
+TAB_RE = re.compile(r'[\t\r\n]+|\s{2,}')  # replaces multiple whitespace characters with ';'
+WSPACE_OP_RE = re.compile(r'\s(([\[\]=|+\-*\/,])+)\s')  # replaces whitespaces on the sides of operator e.g. a = b -> a=b
+WSPACE_DECLARATION_RE = re.compile(r'([\[\]()$,{}])\s([\[\]()$,{}])')  # replaces whitespaces inbetween declarations e.g. function a() {...} -> function a(){...}
+
+FUNC_RE = re.compile(r'function ((\w)+)')  # matches format of function names
+VAR_RE = re.compile(r'\$\w+')  # matches format of PowerShell variable
+
+SPECIAL_VARIABLES = {'$true', '$false'}
 
 
 def minify_PS(contents: str) -> str:
-    multi_tab_regex = r'(\s){2,}'                                # replaces multiple whitespace characters with ';'
-    wspace_operator_regex = r'\s(([\[\]=|+\-*\/,])+)\s'           # replaces whitespaces on the sides of operator e.g. a = b -> a=b
-    wspace_declaration_regex = r'([\[\]()$,{}])\s([\[\]()$,{}])'  # replaces whitespaces inbetween declarations e.g. function a() {...} -> function a(){...}
+    contents = remove_white_space(contents)
 
-    func_regex = r'function ((\w)+)'                             # matches format of function names
-    var_regex = r'\$\w+'                                         # matches format of PowerShell variable
+    script_vars = set(VAR_RE.findall(contents))
+    script_vars -= SPECIAL_VARIABLES
 
-    contents = re.sub(multi_tab_regex, ';', contents)
-    contents = re.sub(wspace_operator_regex, '\g<1>', contents)
-    contents = re.sub(wspace_declaration_regex, '\g<1>\g<2>', contents)
+    contents = replace_with_random_names(contents, script_vars, fake_variable)
 
-    script_vars = set(re.findall(var_regex, contents))
-    script_vars = sorted(script_vars, key=len, reverse=True)
-
-    script_funcs = set(x[0] for x in re.findall(func_regex, contents))
-    script_funcs = sorted(script_funcs, key=len, reverse=True)
-
-    # specify here a list of variables to be ignored upon replacing
-    const_vars = ['$true', '$false']
-
-    for var in const_vars:
-        try:
-            script_vars.remove(var)
-        except ValueError:
-            pass
-
-    for var in script_vars:
-        rand_var = '$' + ''.join(random.choice(rand_namespace) for _ in range(random.randrange(3,10)))
-
-        print('[+] Replacing: {} -> {}'.format(var, rand_var))
-        contents = contents.replace(var, rand_var)
-
-    for func in script_funcs:
-        rand_func = ''.join(random.choice(rand_namespace) for _ in range(random.randrange(3,10)))
-        print('[+] Replacing: {} -> {}'.format(func, rand_func))
-        contents = contents.replace(func, rand_func)
+    script_funcs = set(x[0] for x in FUNC_RE.findall(contents))
+    contents = replace_with_random_names(contents, script_funcs, fake_name)
 
     return contents
+
+
+def replace_with_random_names(text, names_to_replace, name_generator):
+    for func in sorted(names_to_replace, key=len, reverse=True):
+        rand_func = name_generator()
+        logger.debug('[+] Replacing: {} -> {}'.format(func, rand_func))
+        text = text.replace(func, rand_func)
+    return text
+
+
+def remove_white_space(contents):
+    contents = TAB_RE.sub(';', contents)
+    contents = WSPACE_OP_RE.sub('\g<1>', contents)
+    contents = WSPACE_DECLARATION_RE.sub('\g<1>\g<2>', contents)
+    return contents
+
+
+def fake_name():
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(random.randrange(3, 10)))
+
+
+def fake_variable():
+    return '$' + fake_name()
+
